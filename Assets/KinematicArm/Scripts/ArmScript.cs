16f6547cbc1ea72e9152907a6c8a1456727bc099
Redrhino10 <redrhino10.tb@gmail.com>
2020-06-03 17:10:01 +0100

    + Added arm object with Arm script + Fixed the rotation so the arm would rotate around the ArmBase rather than its own origin. + Added a "target" for the arm; this will be reviewed later, but for the time beign is controlled with the keyboard.

---
+++ Assets/KinematicArm/Scripts/ArmScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour
+{
+    
+    private GameObject ArmBase;
+    private GameObject ArmEnd;
+
+    private Vector3 Base = Vector3.zero;
+
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Base = new Vector3(0,0,0);
+
+        transform.position = Base;  //This is necessary it seems...
+
+        ArmBase = gameObject.transform.Find("ArmBase").gameObject;
+        ArmEnd = gameObject.transform.Find("ArmEnd").gameObject;
+
+        transform.position = Base - ArmBase.transform.position;
+
+        Debug.Log("Base = " + ArmBase.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= 0.1f)
+        {
+            RotateTheArm();
+            timer -= 0.1f;
+        }
+    }
+
+    void RotateTheArm()
+    {
+        transform.rotation *= Quaternion.Euler(-5f, -2f, -6f);
+        transform.position -= ArmBase.transform.position;
+    }
+}
---
+++ Assets/KinematicArm/Scripts/TargetMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputs();
+    }
+
+    // Check Q,W,E,A,S,D keys for movement
+    void CheckInputs()
+    {
+        if(Input.GetKey("w"))
+        {
+            gameObject.transform.position += Vector3.up * 0.01f;
+        }
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.position += Vector3.down * 0.01f;
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position += Vector3.left * 0.01f;
+        }
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.position += Vector3.right * 0.01f;
+        }
+        if (Input.GetKey("q"))
+        {
+            gameObject.transform.position += Vector3.forward * 0.01f;
+        }
+        if (Input.GetKey("e"))
+        {
+            gameObject.transform.position += Vector3.back * 0.01f;
+        }
+    }
+
+}
