d2dc4c895b04040511a05b6474df8273d80b978a
Redrhino10 <redrhino10.tb@gmail.com>
2020-06-08 16:21:00 +0100

    - Worked on "ArmScript" for a while, before realising that the arm positions need to be done one-at-a-time for the arm to move properly + moved to the new "ArmManager" which holds an array of structs for the Arm, the Base, the End (socket for next arm) and the socket (end of previous arm OR the base of the whole arm) + Added a vauge rotation to the arms ot show that they can move independantly while remaining attached to one another.

---
+++ Assets/ArmManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct ArmInfo
+    {
+        public GameObject Arm;
+        [HideInInspector]
+        public GameObject ArmBase;  //The joint for the current arm
+        [HideInInspector]
+        public GameObject ArmEnd; //The socket for the next arm
+        [HideInInspector]
+        public Vector3 Socket; //The place it meets the previous arm/base
+    };
+
+    private float Timer = 0.0f;
+    
+    public ArmInfo[] ArmsList = new ArmInfo[0];
+    public GameObject BaseObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //debug for empty array
+        if(ArmsList.Length == 0)
+        {
+            Debug.LogError(gameObject.name + "'s ArmList is empty?");
+            return;
+        }
+
+        //set the base of the arm
+        if (BaseObject == null)
+        {
+            Debug.LogWarning(gameObject.name +
+                " has no BaseObject set; using itself as placeholder");
+            BaseObject = gameObject;
+        }
+
+        //set default armbase and armend values for each arm
+        for (int x = 0; x < ArmsList.Length; x++)
+        {
+            ArmsList[x].ArmBase = ArmsList[x].Arm.transform.Find("ArmBase").gameObject;
+            ArmsList[x].ArmEnd = ArmsList[x].Arm.transform.Find("ArmEnd").gameObject;
+            if (x == 0) { ArmsList[x].Socket = BaseObject.transform.position; }
+            else
+            {
+                ArmsList[x].Socket = ArmsList[x-1].ArmEnd.transform.position;
+            }
+        }
+
+        
+
+        //set initial position for first arm attached to base
+        ArmsList[0].Arm.transform.position += 
+            BaseObject.transform.position - 
+            ArmsList[0].ArmBase.transform.position;
+
+        //set initial positions for other arms to be the end of previous arm
+        //start array at one to be 2nd arm, then x-1 is previous arm
+        for (int x = 1; x < ArmsList.Length; x++)
+        {
+            ArmsList[x].Arm.transform.position +=
+            ArmsList[x-1].ArmEnd.transform.position -
+            ArmsList[x].ArmBase.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= 0.1f)
+        {
+            RotateTheArm();
+            Timer -= 0.1f;
+        }
+    }
+
+    void RotateTheArm()
+    {
+        
+
+        for (int x = 0; x < ArmsList.Length; x++)
+        {
+            ArmInfo ArmX = ArmsList[x];
+
+            if(x == 0)
+            {
+                ArmX.Socket = BaseObject.transform.position;
+            }
+            else
+            {
+                ArmX.Socket = ArmsList[x - 1].ArmEnd.transform.position;
+            }
+
+            ArmX.Arm.transform.rotation *= Quaternion.Euler(-5f * (x + 1), (-2f * (-2 * x)), -6f);
+            ArmX.Arm.transform.position =
+                ArmX.Arm.transform.position -
+                ArmX.ArmBase.transform.position +
+                ArmX.Socket;
+
+        }
+    }
+}
--- Assets/KinematicArm/Scripts/ArmScript.cs
+++ Assets/KinematicArm/Scripts/ArmScript.cs
@@ -7,8 +7,10 @@ public class ArmScript : MonoBehaviour
     
     private GameObject ArmBase;
     private GameObject ArmEnd;
+    private Vector3 PreviousPosition;
 
     private Vector3 Base = Vector3.zero;
+    public GameObject BaseObject;
 
     private float timer = 0;
 
@@ -16,20 +18,46 @@ public class ArmScript : MonoBehaviour
     void Start()
     {
         //Base = new Vector3(0,0,0);
+        if(BaseObject != null)
+        {
+            Base = BaseObject.transform.position;
+        }
+
+        Debug.Log(string.Format("Base is {0} for object {1}", 
+            Base, gameObject)); 
 
         transform.position = Base;  //This is necessary it seems...
+        Debug.Log(string.Format("obj: {0}, tp: {1}, base: {2}",
+            gameObject.name, transform.position, Base));
 
-        ArmBase = gameObject.transform.Find("ArmBase").gameObject;
-        ArmEnd = gameObject.transform.Find("ArmEnd").gameObject;
+        foreach(Transform x in gameObject.transform)
+        {
+            if(x.name == "ArmBase"|| x.name == "ArmBase1") { ArmBase = x.gameObject; }
+            else if(x.name == "ArmEnd" || x.name == "ArmEnd1") { ArmEnd = x.gameObject; }
+        }
+        //ArmBase = gameObject.transform.Find("ArmBase").gameObject;
+        //ArmEnd = gameObject.transform.Find("ArmEnd").gameObject;
 
-        transform.position = Base - ArmBase.transform.position;
+        //transform.position = Base - ArmBase.transform.position;
+        //transform.position -= ArmBase.transform.position;
+        transform.position += Base - ArmBase.transform.position;
 
-        Debug.Log("Base = " + ArmBase.ToString());
+
+        Debug.Log(gameObject.name + "'s Base = " + ArmBase.ToString());
+        //Debug.Log(string.Format("obj: {0}, tp: {1}, base: {2}",
+        //    gameObject.name, transform.position, Base));
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Update base position
+        if(transform.position != Base)
+        {
+            transform.position += Base - ArmBase.transform.position;
+            Debug.Log("XXX");
+        }
+
         timer += Time.deltaTime;
         if(timer >= 0.1f)
         {
@@ -40,7 +68,12 @@ public class ArmScript : MonoBehaviour
 
     void RotateTheArm()
     {
+        Debug.Log(string.Format("Previous Position = {0}, Current = {1}, End = {2}",
+            PreviousPosition, ArmBase.transform.position, ArmEnd.transform.position));
+
+        PreviousPosition = ArmBase.transform.position;
         transform.rotation *= Quaternion.Euler(-5f, -2f, -6f);
-        transform.position -= ArmBase.transform.position;
+        transform.position -= ArmBase.transform.position - PreviousPosition;
+        
     }
 }
